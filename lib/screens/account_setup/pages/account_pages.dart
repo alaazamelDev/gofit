8716de4d3e47104c85d57e1d1f366278325c505d
alaazamelDev <alaa.zamel23@gmail.com>
2022-10-08 20:34:32 +0300

    feat: complete height selection fragment

--- lib/screens/account_setup/pages/account_pages.dart
+++ lib/screens/account_setup/pages/account_pages.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:gofit/screens/account_setup/pages/about_fragment.dart';
 import 'package:gofit/screens/account_setup/pages/age_fragment.dart';
+import 'package:gofit/screens/account_setup/pages/height_fragment.dart';
 import 'package:gofit/screens/account_setup/pages/weight_fragment.dart';
 
 final List<Widget> accountPages = [
   AboutFragment(),
   AgeFragment(),
   WeightFragment(),
+  HeightFragment(),
 ];
