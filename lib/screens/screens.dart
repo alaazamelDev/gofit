e0a9d5de969fa69a6f7a3380d5b128ab836f3b63
alaazamelDev <alaa.zamel23@gmail.com>
2022-10-11 15:14:45 +0300

    fix: upate onboarding screens folder name

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,5 +1,5 @@
 export 'home_screen.dart';
-export 'onboarding_screen/onboarding_screen.dart';
+export 'onboarding/onboarding_screen.dart';
 export 'splash_screen.dart';
 export 'welcome_screen.dart';
 export 'account_setup/account_setup_pages.dart';
